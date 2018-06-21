4067bf2f44131a85a83243a718f9d0c4c400304f
Modesto Rego <mregtej@gmail.com>
2018-06-21 23:31:53 +0200

    Solved TFragments.06-Exercise-CommunicateBetweenActivities

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -26,6 +26,9 @@ import com.example.android.android_me.data.AndroidImageAssets;
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
 
+    private int headIndex;
+    private int bodyIndex;
+    private int legsIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +38,21 @@ public class AndroidMeActivity extends AppCompatActivity {
         // Only create new fragments when there is no previously saved state
         if(savedInstanceState == null) {
 
-            // TODO (5) Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
+            // DONE (5) Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
                 // Use setListindex(int index) to set the list index for all BodyPartFragments
+            Bundle b = getIntent().getExtras();
+            if (b != null) {
+                headIndex = b.getInt("headIndex", 0);
+                bodyIndex = b.getInt("bodyIndex", 0);
+                legsIndex = b.getInt("legsIndex", 0);
+            }
 
             // Create a new head BodyPartFragment
             BodyPartFragment headFragment = new BodyPartFragment();
 
             // Set the list of image id's for the head fragment and set the position to the second image in the list
             headFragment.setImageIds(AndroidImageAssets.getHeads());
-            headFragment.setListIndex(1);
+            headFragment.setListIndex(headIndex);
 
             // Add the fragment to its container using a FragmentManager and a Transaction
             FragmentManager fragmentManager = getSupportFragmentManager();
@@ -56,12 +65,14 @@ public class AndroidMeActivity extends AppCompatActivity {
 
             BodyPartFragment bodyFragment = new BodyPartFragment();
             bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            bodyFragment.setListIndex(bodyIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyFragment)
                     .commit();
 
             BodyPartFragment legFragment = new BodyPartFragment();
             legFragment.setImageIds(AndroidImageAssets.getLegs());
+            legFragment.setListIndex(legsIndex);
             fragmentManager.beginTransaction()
                     .add(R.id.leg_container, legFragment)
                     .commit();
--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,16 +16,23 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity is responsible for displaying the master list of all images
 // Implement the MasterListFragment callback, OnImageClickListener
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
+    private int headIndex;
+    private int bodyIndex;
+    private int legsIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +46,41 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
-        // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        // DONE (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        // headIndex for position [0,11]
+        // bodyIndex for position [12,23]
+        // legIndex for position [24,35]
+        int categoryId = position / 12;
+        switch (categoryId) {
+            case 0: // headIndex
+                headIndex = position;
+                break;
+            case 1: // bodyIndex
+                bodyIndex = position % 12;
+                break;
+            case 2: // legsIndex
+                legsIndex = position % 12;
+                break;
 
-        // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        }
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        // DONE (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        Bundle b = new Bundle();
+        b.putInt("headIndex", headIndex);
+        b.putInt("bodyIndex", bodyIndex);
+        b.putInt("legsIndex", legsIndex);
+
+        final Intent i = new Intent(this, AndroidMeActivity.class);
+        i.putExtras(b);
+
+        // DONE (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
 
     }
 
